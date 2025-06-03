b8ac718e920cc4ee618fff3e65b6cc44dd92966e
Mahir966 <mahirahmed966@gmail.com>
2025-06-03 14:35:43 +0600

    Add files index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Graphic Designer", "Cybersecurity Expert", "Video Editor", "Web Developer", "Ethical Hacker"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true
+});
